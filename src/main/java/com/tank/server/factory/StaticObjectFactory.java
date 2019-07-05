d0e3f11f28c61d1819bb09e951f87e05d335983f
Remco van der Heijden <Remco.van.der.Heijden@philips.com>
2019-07-05 13:35:34 +0200

    Fire

---
+++ src/main/java/com/tank/server/factory/StaticObjectFactory.java
@@ -0,0 +1,33 @@
+package com.tank.server.factory;
+
+import com.tank.server.model.domain.StaticObject;
+
+import lombok.Getter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StaticObjectFactory {
+
+    public enum StaticObjectType {
+        WALL("wall", -1), TREE("tree", 1);
+
+        @Getter
+        private final String type;
+
+        @Getter
+        private final int energy;
+
+        StaticObjectType(final String type, final int energy) {
+            this.type = type;
+            this.energy = energy;
+        }
+
+    }
+
+    public StaticObject createStaticObject(final int[] position, final StaticObjectType type) {
+
+        return new StaticObject(type.getType(), position, type.getEnergy());
+    }
+
+}
--- src/main/java/com/tank/server/service/StateService.java
+++ src/main/java/com/tank/server/service/StateService.java
@@ -1,9 +1,11 @@
 package com.tank.server.service;
 
+import com.tank.server.factory.StaticObjectFactory;
 import com.tank.server.model.ActionRequest;
 import com.tank.server.model.domain.CardinalDirection;
 import com.tank.server.model.domain.Color;
 import com.tank.server.model.domain.Dimension;
+import com.tank.server.model.domain.StaticObject;
 import com.tank.server.model.domain.Tank;
 import com.tank.server.model.domain.TankAction;
 import com.tank.server.model.domain.World;
@@ -13,11 +15,15 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.modelmapper.internal.Pair;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 import java.util.Stack;
 
@@ -27,22 +33,26 @@ public class StateService {
 
     private final Random random;
     private final ServerSettings serverSettings;
+    private final StaticObjectFactory staticObjectFactory;
     private Stack<Tank> availableTanks;
 
     @Getter
     private World world;
 
-    private StateService(final ServerSettings serverSettings) {
+    private StateService(final ServerSettings serverSettings, final StaticObjectFactory staticObjectFactory) {
         this.serverSettings = serverSettings;
+        this.staticObjectFactory = staticObjectFactory;
         random = new Random();
         resetGame();
     }
 
     public void resetGame() {
+
         world =
             new World(
                 new ArrayList<>(),
-                new Dimension(serverSettings.getLevelWidth(), serverSettings.getLevelHeight()));
+                new Dimension(serverSettings.getLevelWidth(), serverSettings.getLevelHeight()),
+                getStaticWorldObjects());
         availableTanks = new Stack<>();
 
         int id = Color.values().length;
@@ -79,6 +89,17 @@ public class StateService {
 
         addTankToWorld(tank);
         return tank;
+    }
+
+    public boolean tankAction(final ActionRequest tankActionRequest) {
+
+        final var action = tankActionRequest.getAction();
+
+        if (action.equals(TankAction.FIRE)) {
+            return tankFire(tankActionRequest.getTankId());
+        } else {
+            return moveTank(tankActionRequest.getTankId(), action);
+        }
     }
 
     private void addTankToWorld(final Tank tank) {
@@ -132,13 +153,6 @@ public class StateService {
         if (tankOpt.isPresent()) {
 
             final var tank = tankOpt.get();
-            final var vector2D = action.getVector2D();
-            final var position = ArrayUtils.clone(tank.getPosition());
-
-            position[0] = position[0] + vector2D.getLeft();
-            position[1] = position[1] + vector2D.getRight();
-
-            // TODO validate if movement is possible
 
             // Check for timed delay
             if (tank.getLastMove() != null &&
@@ -148,31 +162,182 @@ public class StateService {
                 return false;
             }
 
-            tank.setPosition(position);
-            tank.setDirection(CardinalDirection.fromDirection(action.getAction()));
-            tank.setLastMove(Timestamp.from(Instant.now()));
+            final var position = nextPosition(tank.getPosition(), action.getVector2D());
 
+            if (isPositionAvailable(position)) {
+                tank.setPosition(position);
+                tank.setDirection(CardinalDirection.fromDirection(action.getAction()));
+                tank.setLastMove(Timestamp.from(Instant.now()));
+            }
             return true;
         }
 
         return false;
     }
 
+    private int[] nextPosition(final int[] position, Pair<Integer, Integer> vector2D) {
+        final var pos = ArrayUtils.clone(position);
+        pos[0] = pos[0] + vector2D.getLeft();
+        pos[1] = pos[1] + vector2D.getRight();
+        return pos;
+    }
+
     private boolean tankFire(final int tankId) {
 
-        // TODO implement tank fire
+        final var tankOpt = world.getTanks().stream()
+            .filter(tank -> tank.getId() == tankId).findFirst();
+
+        if (tankOpt.isPresent()) {
+            final var tank = tankOpt.get();
+
+            // Check for timed delay
+            if (tank.getLastShot() != null &&
+                tank.getLastShot().toInstant()
+                    .plusSeconds(serverSettings.getTankShootDelay())
+                    .isAfter(Instant.now())) {
+                return false;
+            }
+            tank.setLastShot(Timestamp.from(Instant.now()));
+
+            final var direction = TankAction.fromAction(tank.getDirection().getDirection());
+
+            int[] position = tank.getPosition();
+
+            do {
+                position = nextPosition(position, direction.getVector2D());
+
+                final var optTankShot = getTankAtPosition(position);
+
+                if (optTankShot.isPresent()) {
+
+                    handleTankHit(tank, optTankShot.get());
+                    return true;
+                }
+                final var optStaticObjectShot = collisionWithStaticObject(position);
+
+                if (optStaticObjectShot.isPresent()) {
+
+                    handleStaticObjectHit(optStaticObjectShot.get());
+                    return true;
+                }
+
+            } while (
+                position[0] < serverSettings.getLevelWidth() &&
+                    position[0] > 0 &&
+                    position[1] < serverSettings.getLevelHeight() &&
+                    position[1] > 0
+            );
+
+        }
+
         return false;
     }
 
-    public boolean tankAction(final ActionRequest tankActionRequest) {
+    private void handleTankHit(final Tank shootingTank, final Tank targetTank) {
 
-        final var action = tankActionRequest.getAction();
+        targetTank.setEnergy(targetTank.getEnergy() - 1);
+        shootingTank.getHits().add(targetTank.getId());
 
-        if (action.equals(TankAction.FIRE)) {
-            return tankFire(tankActionRequest.getTankId());
-        } else {
-            return moveTank(tankActionRequest.getTankId(), action);
+        if (targetTank.getEnergy() < 1) {
+
+            shootingTank.getKills().add(targetTank.getId());
+            world.getTanks().remove(targetTank);
+
+            // Free tank to the pool
+            addTankToPool(targetTank);
+        }
+
+    }
+
+    private void handleStaticObjectHit(final StaticObject staticObject) {
+        if (staticObject.getEnergy() > 0) {
+            staticObject.setEnergy(staticObject.getEnergy() - 1);
+
+            if (staticObject.getEnergy() < 1) {
+
+                world.getStaticObjects().remove(staticObject);
+            }
         }
     }
 
+    private Optional<Tank> getTankAtPosition(final int[] position) {
+        return world.getTanks().stream().filter(tank -> Arrays.equals(tank.getPosition(), position)).findFirst();
+    }
+
+    private Optional<StaticObject> collisionWithStaticObject(final int[] position) {
+        return world.getStaticObjects().stream()
+            .filter(staticObject -> Arrays.equals(staticObject.getPosition(), position))
+            .findFirst();
+    }
+
+    private List<StaticObject> getStaticWorldObjects() {
+
+        final var staticObjects = new ArrayList<StaticObject>();
+
+        placeWalls(staticObjects);
+        placeTrees(staticObjects);
+
+        return staticObjects;
+    }
+
+    private void placeWalls(List<StaticObject> staticObjects) {
+
+        for (int x = 0; x < serverSettings.getLevelWidth(); x++) {
+            for (int y = 0; y < serverSettings.getLevelHeight(); y++) {
+
+                final int[] position = new int[] {x, y};
+
+                if (x == 0 ||
+                    x == serverSettings.getLevelWidth() - 1 ||
+                    y == 0 ||
+                    y == serverSettings.getLevelHeight() - 1
+                ) {
+                    staticObjects.add(
+                        staticObjectFactory.createStaticObject(position, StaticObjectFactory.StaticObjectType.WALL)
+                    );
+                }
+
+
+            }
+        }
+    }
+
+    private void placeTrees(List<StaticObject> staticObjects) {
+
+        for (int x = 0; x < serverSettings.getLevelWidth(); x++) {
+            for (int y = 0; y < serverSettings.getLevelHeight(); y++) {
+
+                final int[] position = new int[] {x, y};
+
+                if (
+                    (x > 3 && x < serverSettings.getLevelWidth() - 4 && (y == 3
+                        || y == serverSettings.getLevelHeight() - 4)) ||
+
+                        y > 3 && y < serverSettings.getLevelHeight() - 4 && (x == 3
+                            || x == serverSettings.getLevelWidth() - 4)
+                ) {
+                    staticObjects.add(
+                        staticObjectFactory.createStaticObject(position, StaticObjectFactory.StaticObjectType.TREE)
+                    );
+                }
+            }
+        }
+    }
+
+    private boolean isPositionAvailable(final int[] position) {
+
+        return world.getStaticObjects().stream()
+            .noneMatch(staticObject -> Arrays.equals(staticObject.getPosition(), position)) &&
+            world.getTanks().stream()
+                .noneMatch(tank -> Arrays.equals(tank.getPosition(), position));
+    }
+
+    private void addTankToPool(final Tank tank) {
+
+        tank.setLastMove(null);
+        tank.setLastShot(null);
+        tank.setReloaded(null);
+
+        availableTanks.push(tank);
+    }
 }
